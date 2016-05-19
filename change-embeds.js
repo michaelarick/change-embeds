642eff7ae930ff2d68cd56cdde56584915c0525e
Michael Arick <tech_ops+marick@change.org>
2016-05-19 14:05:40 -0700

    In order to support various sizes, resize on load

--- change-embeds.js
+++ change-embeds.js
@@ -6,6 +6,7 @@ var ChangeEmbeds = function() {
         len;
       for (i = 0, len = divs.length; i < len; i++) {
         ChangeEmbeds.insertIframeIntoDiv(divs[i]);
+        ChangeEmbeds.setEmbedSize(divs[i], 300, 400, 600, 520, 700, 700);
       }
     },
 
@@ -14,9 +15,29 @@ var ChangeEmbeds = function() {
       if (div.getAttribute('data-iframe-loaded') === 'true') {return;}
       var petitionId = div.getAttribute('data-petition-id');
       if (!petitionId) {return;}
-      div.innerHTML = "<iframe height='530px' width='300px' src='https://www.change.org/embed/p/" +
+      div.innerHTML = "<iframe class='iframe-class' width: '300px' height: '520px' src='https://www.change.org/embed/p/" +
         petitionId + "/preview' frameborder='0'></iframe>";
       div.setAttribute('data-iframe-loaded', 'true');
+    },
+
+    setEmbedSize: function(div, minWidth, midWidth, maxWidth, minHeight, midHeight, maxHeight) {
+      var iframe = div.getElementsByClassName('iframe-class')[0];
+      if (!iframe) return;
+      var divWidth = div.offsetWidth,
+        newHeight;
+      if (divWidth <= midWidth) {
+        newHeight = minHeight;
+        newWidth = minWidth;
+      } else if (divWidth < maxWidth) {
+        newHeight = midHeight;
+        newWidth = midWidth;
+      } else {
+        newHeight = maxHeight;
+        newWidth = maxWidth;
+      }
+      div.style.height = newHeight.toString().concat('px');
+      iframe.style.height = newHeight.toString().concat('px');
+      iframe.style.width = newWidth.toString().concat('px');
     }
   }
 }();
